0c41592c97bb9a185bc46ae192c2875818a4b744
serdar <serdarkocerr@gmail.com>
2021-08-22 21:13:15 +0300

    jpa works

---
+++ Spring/RestAndMicroservice/restful-web-services/src/main/java/com/serdar/rest/webservices/restfulwebservices/restfulwebservices/user/Post.java
@@ -0,0 +1,66 @@
+package com.serdar.rest.webservices.restfulwebservices.restfulwebservices.user;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+// relationship between User and Post table. User may have many posts so there is a one-to-many relation.
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String description;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)// this will not retrive detail of user unless you call post to get user.
+	@JsonIgnore // This field ignored returning json, because of preventing recursive call User->post->user->Post....
+	private User user;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", description=" + description + ", user=" + user + "]";
+	}
+	
+	
+	
+	
+	
+}
